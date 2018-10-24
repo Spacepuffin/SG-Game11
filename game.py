0b43a024ef0ef4da85584796e8022f84052b629d
Jerry Guo <Spacepuffin7@gmail.com>
2018-10-24 11:06:00 -0400

    Changes

--- game.py
+++ game.py
@@ -2,18 +2,26 @@ from tkinter import *
 from math import *
 from time import *
 
+root = Tk()
+ctx = Canvas(root, width=1000, height=600, background="black")
+
+
 ##############################
 ####   GLOBAL VARIABLES   ####
 ##############################
-score = 0 #score; also counter for time
-scrSpd = 30 #scroll speed
-entityList = []
+def setInitialValues():
+    global score, scrSpd, entityList, p
+    score = 0 #score; also counter for time
+    scrSpd = 30 #scroll speed
+    entityList = []
+    p = player(0)
 
 ####################
 ####   ENTITY   ####
 ####################
 
 class entity():
+    global entityList
     def __init__(self, id):
         entityList.append(self)
         self.id = id
@@ -24,8 +32,7 @@ class entity():
         self.isFixed = False #determines if it moves relative to everything or not
 
     def draw(self): #draw this obj
-        #draw stuff here
-        e = 0 #filler garbo so it doesn't throw an error
+        ctx.create_oval(self.pos[0]-20,self.pos[1]-20,self.pos[0]+20,self.pos[1]+20, fill="blue")
     def update(self):
         if self.isFixed:
             self.pos[0] -= scrSpd
@@ -33,7 +40,6 @@ class entity():
             self.pos[0] += self.vel[0]
             self.pos[1] += self.vel[1]
         self.draw()
-        print(self.id, "updated")
         
 
 ####################
@@ -50,17 +56,22 @@ class player(entity):
         #shooty stuff here
         entity.update(self)
 
-p = player(0)
 ##################
 ####   MAIN   ####
 ##################
 
 def mainUpdate(): #updates everything
+    global entityList
     for i in range(len(entityList)): #loops through the list of entities and calls update in all of them
-        entityList[i].update
+        entityList[i].update()
         
 def runGame():
+    setInitialValues()
     while True:
         mainUpdate()
+        
 
 runGame()
+screen.pack()
+screen.focus_set()
+root.mainloop()
