cd356607d0d9cf9fccb8b43264f2b360211762ca
Jerry Guo <Spacepuffin7@gmail.com>
2019-01-17 11:59:37 -0500

    Update game.py

--- game.py
+++ game.py
@@ -20,7 +20,10 @@ def setInitialValues():
     score = 0 #score; also counter for time
     scrSpd = 25 #scroll speed
     gravity = 2
-    platGen = [1,1,1,1] #Whether each row is producing platforms
+    platGen = []#Whether each row is producing platforms
+    for i in range(4):
+        platGen.append(choice([-2,-5,-13]))
+        
     #Lists of Object instances:
     global entityList, platformList, projectileList, particleList
     entityList = []
@@ -67,10 +70,7 @@ def setInitialValues():
 
     #Platforms at the Start of the Game:
     for i in range(26):
-        platform(350,i*50+5)
         platform(550,i*50+5)
-        platform(750,i*50+5)
-        platform(950,i*50+5)
         
 def pVector(magnitude, angle):
 	x = magnitude*cos(angle)
@@ -107,15 +107,15 @@ class entity():
 class player(entity):
     def __init__(self):
         entity.__init__(self)
-        self.pos = [400, 300]
-        self.shotCharge = 0
+        self.pos = [400, 525]
+        self.shotCharge = 100
         self.health = 5
-        self.invulnerable = False
+        self.invulnerable = 0
         self.moveSpeed = 10
         self.angleMouse = 0
         
     def draw(self):
-        if not (self.invulnerable and t%4 < 2): #blink if invulnerable
+        if not (self.invulnerable > 0 and t%4 < 2): #blink if invulnerable
             if self.airborne:
                 if self.vel[1] > 0:
                     ctx.create_image(self.pos[0], self.pos[1], image = pBodyImgs[13], anchor = CENTER)
@@ -161,13 +161,15 @@ class player(entity):
             recoil = pVector(30, self.angleMouse + pi)#launch self with 30 force away from your mouse
             self.vel[0] += recoil[0]
             self.vel[1] += recoil[1]
-            for i in range(randint(3,5)):
+            for i in range(5):
                 projectile()
             self.shotCharge -= 100
             
     def update(self):
-        if self.shotCharge <= 200:
+        if self.shotCharge < 200:
             self.shotCharge += 2
+            if not self.airborne: #Charge more while on the ground
+                self.shotCharge += 0.5
         
         self.angleMouse = atan2(yMouse - self.pos[1], xMouse - self.pos[0])
         self.checkCollisions()
@@ -186,6 +188,16 @@ class player(entity):
         if self.pos[1] >= 1100:  #If you fall off the stage:
             self.pos = [400, -50]
             self.vel = [0,0]
+            if not tutorial > 0:
+                self.health -= 1
+                self.invulnerable = 60 #give 60 ticks of invulnerable after falling off the stage.
+            
+        if self.invulnerable > 0: 
+            self.invulnerable -= 1 # tick down the invuln timer if you're invulnerable
+
+
+        if self.health <= 0:
+            endGame()
         
         entity.update(self)
         self.draw()
@@ -199,11 +211,11 @@ class projectile(entity):
         projectileList.append(self)
         entity.__init__(self)
         self.pos = [p.pos[0],p.pos[1]]
-        self.vel = pVector(30 + uniform(-5,5), p.angleMouse + uniform(-0.5,0.5))
+        self.vel = pVector(30 + uniform(-5,5), p.angleMouse + uniform(-pi/4,pi/4))
         self.rad = randint(13,30)
         
     def draw(self):
-        ctx.create_oval(self.pos[0]-self.rad,self.pos[1]-self.rad,self.pos[0]+self.rad,self.pos[1]+self.rad, fill = "#da9d3f", outline = "#da9d3f")
+        ctx.create_oval(self.pos[0]-self.rad,self.pos[1]-self.rad,self.pos[0]+self.rad,self.pos[1]+self.rad, fill = "#bf8d5f", outline = "#da9d3f")
         
     def update(self):
         self.draw()
@@ -269,6 +281,7 @@ def pauseGame():
     global game, quitButton, pauseButton, resetButton
     if game:
         game = False
+        ctx.create_rectangle(0,0,1280,1024,fill="gray10",outline="",stipple="gray50")
         ctx.create_text(640,300, text = "- p a u s e -", font = ("Small Fonts", 112, "bold"), fill = "White")
         quitButton.place(x=350, y=600, width=150, height=60)
         pauseButton.place(x=540, y=600, width=200, height=60)
@@ -278,6 +291,16 @@ def pauseGame():
         quitButton.place(x=-200, y=-200, width=150, height=60) # Hide the buttons when not paused
         pauseButton.place(x=-200, y=-200, width=150, height=60)
         resetButton.place(x=-200, y=-200, width=150, height=60)
+
+##   End Game Function   ##
+def endGame():
+    global game, quitButton, resetButton
+    game = False
+    
+    ctx.create_rectangle(0,0,1280,1024,fill="black",outline="",stipple="gray50")
+    ctx.create_text(640,400, text = "- t r y    a g a i n ? -", font = ("Small Fonts", 112, "bold"), fill = "White")
+    quitButton.place(x=350, y=600, width=150, height=60)
+    resetButton.place(x=780, y=600, width=150, height=60)
 
 ##   Reset The Game   ##
 
@@ -292,33 +315,65 @@ def resetGame(): #Essentially just setInitialValues but it also moves the button
 
 def drawHUD():
     global score, p
+    #Tutorial Images
+    ctx.create_image(self.pos[0], self.pos[1], image = pBodyImgs[13], anchor = CENTER) AAAAAAaa
+
+    #Ammo Gauge
     gaugeColour = ""
     if (p.shotCharge >= 100):
-        gaugeColour = "#da9d3f"
+        gaugeColour = "#bf8d5f"
     else:
         gaugeColour = "grey60"
-        
-        
-    ctx.create_rectangle(30,230, 60, 230-(p.shotCharge), fill = gaugeColour, outline = "white", width = 3)
-    ctx.create_polygon(63,130,70,135,70,125, fill="white", outline="white")
+    ctx.create_rectangle(30,330, 70, 330-(p.shotCharge * 1.5), fill = gaugeColour, outline = "white", width = 4)
+    ctx.create_polygon(67,185,55,190,55,180, fill="white", outline="white")
+
+    #Health Indicator
+    ctx.create_text(100,30, text = "Health:",font = ("Small Fonts", 36, "bold"), anchor = NW, fill = "White")
+    for i in range(p.health):
+        ctx.create_rectangle(260+50*i, 45, 300+50*i, 75, fill = "white", outline  = "white")
+
+    #Score Value
+    ctx.create_text(1250 - (160 + len(str(score))*20), 30, text = "Score: " + str(score),font = ("Small Fonts", 36, "bold"), anchor = NW, fill = "White")
 
 ##   Platform Layout Generator   ##
 def platGenerator():
     global platGen
-    platChoices = [5]*3 + [8]*2 + [10] + [-5]*3 + [-8] #in the beginning, there are many platforms.
+    platChoices = [5] + [8]*2 + [13]*4 + [21]*2 + [26] #At the start, there is a higher chance for longer platforms
+    gapChoices = [3] + [5]*2 + [8] 
+
+    if score >= 500:
+        platChoices += [5]
+        platChoices.remove(26)
+        gapChoices += [8]
+        gapChoices.remove(3)
+    if score >= 1500:
+        platChoices += [3]
+        platChoices.remove(21)
+        platChoices.remove(13)
+        gapChoices += [13]
+        gapChoices.remove(5)
+    if score >= 3000:
+        platChoices += [3]
+        platChoices.remove(13)
+        platChoices.remove(13)
+        gapChoices += [13] + [21]*2
+        gapChoices.remove(5)
     
     if (t%2==0):# Every other frame:
-        for i,row in enumerate(platGen):
-            if row > 1:
-                platform((i+1)*200+150)
-                platGen[i] -=1
-            elif row < -1:
-                platGen[i] += 1
-            elif row == 1:
-                platform((i+1)*200+150)
-                platGen[i] = choice(platChoices)
-            else:
-                platGen[i] = choice(platChoices)
+        if tutorial>0: #if it's the tutorial, only make platforms on one level with no gaps
+            platform(550)
+        else:
+            for i,row in enumerate(platGen):
+                if row > 1:
+                    platform((i+1)*200+150)
+                    platGen[i] -=1
+                elif row < -1:
+                    platGen[i] += 1
+                elif row == 1:
+                    platform((i+1)*200+150)
+                    platGen[i] = choice(gapChoices)*-1 #alternate between platforms and gaps.
+                else:
+                    platGen[i] = choice(platChoices)
 
 ##   Main Update   ##
 def mainUpdate(): #updates everything
@@ -384,6 +439,7 @@ def keyUpHandler( event ):
     
 def runGame():
     setInitialValues()
+    global tutorial = 2
     while True:
         if game:
             ctx.delete(ALL)
@@ -391,6 +447,7 @@ def runGame():
             drawHUD()
         ctx.update()
         sleep(1/30)
+    
 
 root.after( 0, runGame )
 
